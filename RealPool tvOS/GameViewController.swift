248e7cd709d01d2e986a348452858701542c31c7
Alex Bater <bater.a@etoncollege.org.uk>
2023-09-18 12:40:29 +0100

    Initial Commit

---
+++ RealPool tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  RealPool tvOS
+//
+//  Created by Bater, Alexander (IRG) on 18/09/2023.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
