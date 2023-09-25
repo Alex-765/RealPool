a00d99673ff74b423c55d27f2bfbc2a488e7fe59
alexanderbater <bater.a@etoncollege.org.uk>
2023-09-25 12:28:13 +0100

    more

--- RealPool Shared/GameScene.swift
+++ RealPool Shared/GameScene.swift
@@ -17,15 +17,23 @@ class GameScene: SKScene {
         
         physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
         //        Gravity does not play a significant role in a pool game, as 2d
-        
-        var balls = ["RedBall", "BlueBall"]
-        
+
         let table = SKSpriteNode(imageNamed: "PoolTable")
-        
         let sceneSize = CGSize(width: 1280, height: 720)
         let gameScene = GameScene(size: sceneSize)
         table.position = CGPoint(x: 640, y: 360)
+        table.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        table.zPosition = -1
         addChild(table)
+        
+        //        Table Sprite create and set up
+        
+        let redBallTexture = SKTexture(imageNamed: "RedBall")
+        let blueBallTexture = SKTexture(imageNamed: "BlueBall")
+        
+        
+        
+        
     }
 
     override func update(_ currentTime: TimeInterval) {
